ec17ff1f04fd0f3d5fc06a628e8d0c8c8d8be659
jbilander <jorgen.bilander@gmail.com>
2023-05-15 16:02:36 +0200

    Added Common.h with CommandEnum etc.

---
+++ Common.h
@@ -0,0 +1,55 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <Arduino.h>
+#include <list>
+#include <map>
+#include <vector>
+
+namespace util
+{
+    template <typename T>
+    T Split(String str, char parser)
+    {
+        T str_array;
+        int last_idx = 0;
+        int next_idx = str.indexOf(parser, last_idx);
+        do
+        {
+            str_array.push_back(str.substring(last_idx, next_idx));
+            last_idx = next_idx + 1;
+            next_idx = str.indexOf(parser, last_idx);
+            if (next_idx == -1 && last_idx != 0)
+            {
+                str_array.push_back(str.substring(last_idx, str.length()));
+            }
+        } while (next_idx != -1);
+        return str_array;
+    }
+}
+
+enum ClientEnum
+{
+    UART,
+    TELNET
+};
+
+enum CommandEnum
+{
+    LS,
+    DIR,
+    EXIT
+};
+
+struct CommandMap : public std::map<String, CommandEnum>
+{
+    CommandMap()
+    {
+        this->operator[]("LS") = LS;
+        this->operator[]("DIR") = DIR;
+        this->operator[]("EXIT") = EXIT;
+    };
+    ~CommandMap() {}
+};
+
+#endif
